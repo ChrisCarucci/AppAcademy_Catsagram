06e307dff849b4b3d187d529026a49c7b8e02972
Chris <Chris.W.Carucci@Gmail.com>
2022-12-01 21:44:00 -0330

    Update score.js

--- public/score.js
+++ public/score.js
@@ -1,91 +1,81 @@
 export const createScoreContainer = () => {
-
-    // Create Container for Score, Upvotes, Comments
-
+    // Create score container
     const scoreContainer = document.createElement("div");
     scoreContainer.className = "score-container";
     scoreContainer.style.display = "flex";
-    scoreContainer.style.direction = "column";
+    scoreContainer.style.flexDirection = "column";
     scoreContainer.style.alignItems = "center";
 
-    const scoreDisplay = createScoreDisplay;
-    const btnContainer = createBtnContainer;
+    const scoreDisplay = createScoreDisplay();
+    const btnContainer = createBtnContainer();
 
     scoreContainer.appendChild(scoreDisplay);
-    scoreContainer.appendChild(btnContainer)
-
+    scoreContainer.appendChild(btnContainer);
 
     const container = document.querySelector(".container");
-    container.appendChild(scoreContainer)
-    
+    container.appendChild(scoreContainer);
+};
 
-}
-
-    // Create Score Container
 const createScoreDisplay = () => {
-    const scoreDisplay = document.createComment("div");
+    // Create score display
+    const scoreDisplay = document.createElement("div");
     scoreDisplay.className = "score-display";
     scoreDisplay.style.marginBottom = "10px";
 
     const scoreTitle = document.createElement("span");
-    scoreTitle.innerText = "Current Votes";
+    scoreTitle.innerText = "Popularity Score: ";
 
-    const score = document.createElement("span")
+    const score = document.createElement("span");
     score.className = "score";
     score.innerText = "0";
 
     scoreDisplay.appendChild(scoreTitle);
-    scoreDisplay.appendChild(score)
+    scoreDisplay.appendChild(score);
 
     return scoreDisplay;
-}
+};
 
-    // Create Button Container
-    const createBtnContainer = () => {
+const createBtnContainer = () => {
+    // Create upvote/downvote buttons
     const btnContainer = document.createElement("div");
 
     const upvoteBtn = document.createElement("button");
     upvoteBtn.id = "upvote";
-    upvoteBtn.innerText = "Upvote!"
+    upvoteBtn.innerText = "Upvote";
 
     const downvoteBtn = document.createElement("button");
     downvoteBtn.id = "downvote";
-    downvoteBtn.innerText = "Downvote! :(";
+    downvoteBtn.innerText = "Downvote";
     downvoteBtn.style.marginLeft = "5px";
 
-    btnContainer.appendChild(upvoteBtn)
-    btnContainer.appendChild(downvoteBtn)
+    btnContainer.appendChild(upvoteBtn);
+    btnContainer.appendChild(downvoteBtn);
 
-    upvoteBtn.addEventListener("click", vote);
-    downvoteBtn.addEventListener("click", vote);
+    upvoteBtn.addEventListener('click', vote);
+    downvoteBtn.addEventListener('click', vote);
 
     return btnContainer;
-}
+};
 
-
-// Create System for voting on click part I.
 const vote = e => {
-    const score = document.querySelector(".score")
+    const score = document.querySelector('.score');
     let newScore = score.innerText;
-
     if (e.target.id === "upvote") {
         newScore = parseInt(newScore) + 1;
     } else {
         newScore = parseInt(newScore) - 1;
     }
-
+    
+    // update score
     updateScore(newScore);
-}
-
-
-// Create System for voting on click part II.
+};
 
 export const resetScore = () => {
+    // reset score to 0
     updateScore(0);
-}
+};
 
 const updateScore = (newScore) => {
     const score = document.querySelector('.score');
     score.innerText = newScore;
-}
-
+};
